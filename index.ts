97852b65b38279daf1e237412f3d0c80ef504951
alexander spevak <alexander.spevak@economia.cz>
2019-08-20 19:51:58 +0200

    feat: first commit, created function for objectparsing

---
+++ index.ts
@@ -0,0 +1,16 @@
+const objectCreator =(objectMap: string[], startObject = {}) => {
+  const depth = objectMap.length
+  const key = objectMap[0]
+  if(depth === 1) {
+    startObject[key] = null
+    return startObject
+  }
+  const nextLevelMap = [...objectMap]
+  nextLevelMap.shift()
+  startObject[key] = objectCreator(nextLevelMap, startObject[key])
+
+  return startObject
+}
+
+console.log(JSON.stringify(objectCreator(['ahoj', 'cau', 'sevas',], {zdravim: 'neco', ahoj:{pozdrav: 'poz'}})))
+
